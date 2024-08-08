a7918d98e29db605fd2b6233f786d55a43a63ada
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-08-08 23:46:09 +0700

    Update Not Found View

---
+++ airline/src/views/ResourceNotFound.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+const props = withDefaults(
+    defineProps<{
+        resource: string
+    }>(),
+    {
+    resource: 'page'
+    }
+)
+</script>
+
+<template>
+    <h1>Resource Not Found</h1>
+    <h3>The {{ resource }} you're looking for is not here.</h3>
+    <router-link :to="{ name: 'event-list-view' }">Back to the home page</router-link>
+</template>
---
+++ airline/src/views/event/DetailPassView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { type Passen } from '@/types'
+import PassService from '@/services/PassService'
+import { useRouter } from 'vue-router'
+
+const event = ref<Passen | null>(null)
+const props = defineProps ({
+    id: {
+        type: String,
+        required: true
+    }
+})
+const router = useRouter ()
+onMounted (() => {
+    PassService.getEvent(parseInt(props.id))
+        .then((response) => {
+            event.value = response.data
+        })
+        .catch((error) => {
+            if (error.respose && error.response.status === 404) {
+            router.push ({
+                name: '404-resource-view',
+                params: { resource: 'event' }
+            })
+            } else {
+                router.push ({ name: 'network-error-view' })
+            }
+        })
+})
+</script>
+<template>
+    <div v-if="event">
+        <h1>{{ event.title }}</h1>
+        <nav>
+            <RouterLink :to="{ name: 'event-detail-view' }">Details</RouterLink>
+            |
+            <RouterLink :to="{ name: 'event-register-view' }">Register</RouterLink>
+            |
+            <RouterLink :to="{ name: 'event-edit-view' }">Edit</RouterLink>
+        </nav>
+        <RouterView :event="event" />
+    </div>
+</template>
+
